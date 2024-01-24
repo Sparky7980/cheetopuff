01423fd621005b731e6e5f6667b76f61364cdd23
Sparky7980 <133839013+Sparky7980@users.noreply.github.com>
2024-01-23 17:56:58 -0800

    Update hack.js

--- hack.js
+++ hack.js
@@ -4,5 +4,4 @@ localStorage.setItem("totalKills", 98765);
 localStorage.setItem("coinsOwned", 87687677432);
 localStorage.setItem("abBotSkillLevel", 1);
 localStorage.setItem("bestGameTime", 5m 21s);
-localStorage.setItem("unlockedSkins", JSON.stringify([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35]));
-localStorage.setItem("unlockedPets", JSON.stringify([1, 2, 3, 4, 5, 6, 7, 8, 9]));
+alert("Reload the page to save")
