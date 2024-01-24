fff50e62a5a4cf65ba025e93faf40958353d9db8
Sparky7980 <133839013+Sparky7980@users.noreply.github.com>
2024-01-23 16:59:03 -0800

    Create hack.js

---
+++ hack.js
@@ -0,0 +1,7 @@
+console.log("Script By Sparky7980")
+localStorage.setItem("totalWins", 968);
+localStorage.setItem("totalKills", 98765);
+localStorage.setItem("coinsOwned", 87687677432);
+localStorage.setItem("username", hecker);
+localStorage.setItem("unlockedSkins", 	[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]);
+isl = prompt("What island do you want to go to?")
