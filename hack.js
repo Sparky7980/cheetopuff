8795835fa7b3a7e8d41c3e5e1874c744dc5f307f
Sparky7980 <133839013+Sparky7980@users.noreply.github.com>
2024-01-23 17:50:02 -0800

    Update hack.js

--- hack.js
+++ hack.js
@@ -2,10 +2,7 @@ console.log("Script By Sparky7980")
 localStorage.setItem("totalWins", 968);
 localStorage.setItem("totalKills", 98765);
 localStorage.setItem("coinsOwned", 87687677432);
-var b = prompt("What character would you like? 1-35");
-        if(isNaN(b) || b < 1 || b > 35){
-            window.alert("Please put a number from 1-35 next time.");
-        }else if(b != null || b != undefined || b != ""){
-            localStorage.setItem("playerSkin", b);
-            window.alert("Please reload to enable the skin");
-isl = prompt("What island do you want to go to?")
+localStorage.setItem("abBotSkillLevel", 1);
+localStorage.setItem("bestGameTime", 5m 21s);
+localStorage.setItem("unlockedSkins", JSON.stringify([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35]));
+localStorage.setItem("unlockedPets", JSON.stringify([1, 2, 3, 4, 5, 6, 7, 8, 9]));
