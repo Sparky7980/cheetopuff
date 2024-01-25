b20df3a9ad7ecac3a84f3ad4b374b4030bc255d5
Sparky7980 <133839013+Sparky7980@users.noreply.github.com>
2024-01-24 16:29:21 -0800

    Delete yohoho.js

--- yohoho.js
+++
@@ -1,7 +0,0 @@
-console.log("Script By Sparky7980")
-localStorage.setItem("totalWins", 968);
-localStorage.setItem("totalKills", 98765);
-localStorage.setItem("coinsOwned", 87687677432);
-localStorage.setItem("abBotSkillLevel", 1);
-localStorage.setItem("bestGameTime", 5m 21s);
-alert("Reload the page to save")
